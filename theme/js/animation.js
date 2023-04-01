b62b3cacacc84f6d393daea121a39e9bdecb313d
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2023-04-01 11:49:40 +0200

    Simplify animation.js and remove jquery completely

--- theme/js/animation.js
+++ theme/js/animation.js
@@ -1,8 +1,8 @@
 // Animation.js
 // Set the transition time
 const animationPageDuration = 250;
-var animationTimeout;
-var hidePages = [""];
+let animationTimeout;
+let hidePages = [""];
 
 // Set the z displacement
 const zDisplacement = 100;
@@ -12,8 +12,7 @@ const allPages = document.querySelectorAll("#main .page");
 
 // Function to animate the frame transitions
 function animatePage(whichFrame) {
-  const html = document.querySelector("html");
-  html.classList.add("hide_scroll");
+  document.querySelector("html").classList.add("hide_scroll");
 
   whichPageActive = whichFrame;
 
@@ -25,25 +24,22 @@ function animatePage(whichFrame) {
       allPages[j - 1].style.display = "block";
       allPages[j - 1].style.zIndex = 0;
 
-      $("#page_" + pages[j - 1])
-        .delay(0)
-        .queue(function (next) {
-          $(this).css({
-            opacity: 1,
-            transform: "translateX(0) translateY(0) translateZ(0)",
-            transition: "all " + animationPageDuration + "ms",
-          });
-          next();
-        });
+      setTimeout(() => {
+        allPages[j - 1].style.opacity = 1;
+        allPages[j - 1].style.transform =
+          "translateX(0) translateY(0) translateZ(0)";
+        allPages[j - 1].style.transition =
+          "all " + animationPageDuration + "ms";
+      }, 0);
 
-      const navUlLiA = document.querySelectorAll("#nav ul li a");
-      navUlLiA.forEach((e) => e.classList.remove("activeMenu"));
-      const navUlLinth = document.querySelectorAll(
-        "#nav ul li:nth-child(" + (whichFrame + 1) + ") a"
-      );
-      navUlLinth.forEach((e) => e.classList.add("activeMenu"));
+      document
+        .querySelectorAll("#nav ul li a")
+        .forEach((e) => e.classList.remove("activeMenu"));
+      document
+        .querySelectorAll("#nav ul li:nth-child(" + (whichFrame + 1) + ") a")
+        .forEach((e) => e.classList.add("activeMenu"));
     } else if (whichFrame > j) {
-      allPages[j - 1].style.opcaity = 0;
+      allPages[j - 1].style.opacity = 0;
       allPages[j - 1].style.transform =
         "translateX(0) translateY(0) translateZ(" +
         zDisplacement * (whichFrame - j) +
@@ -55,24 +51,12 @@ function animatePage(whichFrame) {
       hideCounter++;
     } else {
       let opacityValue = (1 / (j - whichFrame)) * 0.1;
-      if ($("#main .page:nth-child(" + j + "):visible").length == 0) {
-        allPages[j - 1].style.opacity = opacityValue;
-        allPages[j - 1].style.transform =
-          "translateX(0) translateY(0) translateZ(" +
-          zDisplacement * (whichFrame - j) +
-          "px)";
-        allPages[j - 1].style.transition =
-          "all " + animationPageDuration + "ms";
-      } else {
-        allPages[j - 1].style.opacity = opacityValue;
-        allPages[j - 1].style.transform =
-          "translateX(0) translateY(0) translateZ(" +
-          zDisplacement * (whichFrame - j) +
-          "px)";
-        allPages[j - 1].style.transition =
-          "all " + animationPageDuration + "ms";
-      }
-
+      allPages[j - 1].style.opacity = opacityValue;
+      allPages[j - 1].style.transform =
+        "translateX(0) translateY(0) translateZ(" +
+        zDisplacement * (whichFrame - j) +
+        "px)";
+      allPages[j - 1].style.transition = "all " + animationPageDuration + "ms";
       allPages[j - 1].style.zIndex = -(j - 1 + 1);
 
       hide[hideCounter] = "#page_" + pages[j - 1];
