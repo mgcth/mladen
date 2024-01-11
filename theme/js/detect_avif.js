925b2dc40fad583e0b253514bf4d5ac567311a0d
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-01-11 23:08:22 +0100

    Add P3 color check and add info about avif and p3

--- theme/js/detect_avif.js
+++ theme/js/detect_avif.js
@@ -1,9 +1,23 @@
-var AVIF1 = new Image();
-  AVIF1.onload = function () {
-    document.getElementById("avif-element").classList.add("avif-true")
-  };
-  AVIF1.onerror = function () {
-    document.getElementById("avif-element").classList.add("avif-false")
-  };
-  AVIF1.src =
-    "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
+const AVIF = new Image();
+AVIF.onload = function () {
+  document.getElementById("avif-element").classList.add("avif-true")
+  document.getElementById("avif-element").setAttribute("title", "Your browser supports the AVIF format!")
+};
+AVIF.onerror = function () {
+  document.getElementById("avif-element").classList.add("avif-false")
+  document.getElementById("avif-element").setAttribute("title", "No support for AVIF format, pictures won't show.")
+};
+AVIF.src =
+  "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
+
+const P3 = window.matchMedia('(color-gamut: p3)').matches
+if (P3 === false) {
+  document.getElementById("color-p3").classList.remove("color-true-p3")
+  document.getElementById("color-srgb").classList.remove("color-true-srgb")
+  document.getElementById("color-p3").classList.add("color-false-p3")
+  document.getElementById("color-srgb").classList.add("color-false-p3")
+
+  document.getElementById("color-gamut").setAttribute("title", "No support for P3 color space, pictures will look oversaturated.")
+} else {
+  document.getElementById("color-gamut").setAttribute("title", "Your browser supports the P3 color space!")
+}
