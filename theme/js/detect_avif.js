eb558353afefc8e7607ea30fea6edf456c8f3834
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-01-11 11:26:50 +0100

    Add detection of avif and update p3 detection

---
+++ theme/js/detect_avif.js
@@ -0,0 +1,9 @@
+var AVIF1 = new Image();
+  AVIF1.onload = function () {
+    document.getElementById("avif-element").classList.add("avif-true")
+  };
+  AVIF1.onerror = function () {
+    document.getElementById("avif-element").classList.add("avif-false")
+  };
+  AVIF1.src =
+    "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
